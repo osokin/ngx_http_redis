a111fab56579145b247c87064aaf11b0d1ad6f06
Sergey A. Osokin <osa@FreeBSD.org.ru>
2025-05-18 10:37:35 -0400

    Upstream: the "transparent" parameter of redis_bind
    https://github.com/nginx/nginx/commit/be79f5cb16eeb452e2a9e343a89f89b3b47bc5a2

--- ngx_http_redis_module.c
+++ ngx_http_redis_module.c
@@ -71,7 +71,7 @@ static ngx_command_t  ngx_http_redis_commands[] = {
       NULL },
 
     { ngx_string("redis_bind"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE12,
       ngx_http_upstream_bind_set_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_redis_loc_conf_t, upstream.local),
